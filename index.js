4a89106f9f5d738009c83591c6bb696a9e166430
Mike Loseto <losetom1998@gmail.com>
2022-07-17 10:58:20 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(funTime = 'roller-skate') {
+    return `This Saturday, I want to ${funTime}!`
+}
+
+function mondayWork(homeWork = 'go to the office') {
+    return `This Monday, I will ${homeWork}.`
+}
+
+function wrapAdjective(adjWrap = '*') {
+    function inner(text = 'special') {
+        return `You are ${adjWrap}${text}${adjWrap}!`
+    }
+    return inner 
+}
+
